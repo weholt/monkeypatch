58981cb2b32507d483597829b646bb29785e6196
Thomas Weholt <2002@weholt.org>
2024-05-30 00:25:24 +0200

    Lots of updates.

--- monkeypatch/apps.py
+++ monkeypatch/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MonkeypatchConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'monkeypatch'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "monkeypatch"
